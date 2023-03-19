27f7da28d45707ba72d27952d225eb631549a0d5
ImproperIssues <114215797+ItziSpyder@users.noreply.github.com>
2023-03-18 23:39:56 -0700

    Add files via upload

--- src/main/java/io/github/itzispyder/clickcrystals/modules/modules/GlowStoneSearch.java
+++ src/main/java/io/github/itzispyder/clickcrystals/modules/modules/GlowStoneSearch.java
@@ -9,6 +9,7 @@ import io.github.itzispyder.clickcrystals.util.HotbarUtils;
 import io.github.itzispyder.clickcrystals.util.Randomizer;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.RespawnAnchorBlock;
 import net.minecraft.item.Items;
 import net.minecraft.network.packet.c2s.play.PlayerInteractBlockC2SPacket;
 
@@ -38,13 +39,22 @@ public class GlowStoneSearch extends Module implements Listener {
         if (!super.isEnabled()) return;
         if (e.getPacket() instanceof PlayerInteractBlockC2SPacket packet) {
             BlockState state = mc.player.getWorld().getBlockState(packet.getBlockHitResult().getBlockPos());
-
             if (state == null) return;
+            if (!mc.options.useKey.isPressed()) return;
+
             if (state.isOf(Blocks.RESPAWN_ANCHOR)) {
-                if (!mc.options.useKey.isPressed()) return;
-                if (!HotbarUtils.isHolding(Items.GLOWSTONE)) return;
+                int charges = state.get(RespawnAnchorBlock.CHARGES); // charges of the clicked block
+                if (HotbarUtils.isHolding(Items.GLOWSTONE) && charges > 0) { // after charging the anchor
+                    new ScheduledTask(() -> {
+                        HotbarUtils.search(Items.RESPAWN_ANCHOR);
+                    }).runDelayedTask(Randomizer.rand(50));
+                    return;
+                }
+            }
+
+            if (HotbarUtils.isHolding(Items.RESPAWN_ANCHOR) && !state.isOf(Blocks.RESPAWN_ANCHOR)) {
                 new ScheduledTask(() -> {
-                    HotbarUtils.search(Items.RESPAWN_ANCHOR);
+                    HotbarUtils.search(Items.GLOWSTONE);
                 }).runDelayedTask(Randomizer.rand(50));
             }
         }
--- src/main/java/io/github/itzispyder/clickcrystals/util/BlockUtils.java
+++ src/main/java/io/github/itzispyder/clickcrystals/util/BlockUtils.java
@@ -1,10 +1,13 @@
 package io.github.itzispyder.clickcrystals.util;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
 
 import static io.github.itzispyder.clickcrystals.ClickCrystals.mc;
 
@@ -14,12 +17,24 @@ import static io.github.itzispyder.clickcrystals.ClickCrystals.mc;
 public abstract class BlockUtils {
 
     public static ActionResult interact(Vec3d vec) {
+        return interact(vec,Direction.UP);
+    }
+
+    public static ActionResult interact(Vec3d vec, Direction dir) {
         BlockPos pos = new BlockPos(vec);
-        BlockHitResult result = new BlockHitResult(vec, Direction.UP,pos,false);
+        BlockHitResult result = new BlockHitResult(vec, dir,pos,false);
         return mc.interactionManager.interactBlock(mc.player,mc.player.getActiveHand(),result);
     }
 
     public static ActionResult interact(BlockHitResult result) {
         return mc.interactionManager.interactBlock(mc.player,mc.player.getActiveHand(),result);
+    }
+
+    public static boolean matchTargetBlock(Block block) {
+        World world = mc.player.getWorld();
+        Vec3d vec = mc.crosshairTarget.getPos();
+        BlockPos pos = new BlockPos(vec);
+        BlockState state = world.getBlockState(pos);
+        return state != null && state.isOf(block);
     }
 }
