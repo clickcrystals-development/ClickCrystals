a4280e3f63b5a14e4283e5c77e6299e491e11fbb
ImproperIssues <114215797+ItziSpyder@users.noreply.github.com>
2023-03-22 17:48:20 -0700

    Delete src/main directory

--- src/main/java/io/github/itzispyder/clickcrystals/events/Listener.java
+++
@@ -1,9 +0,0 @@
-package io.github.itzispyder.clickcrystals.events;
-
-/**
- * Listener interface, apply this to objects that contains events
- * for the listener.
- */
-public interface Listener {
-
-}
