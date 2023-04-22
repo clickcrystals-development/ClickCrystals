79f01be47e4986a2bd5bd2712588be69dd2c1fe1
ImproperIssues <ItziSpyder@gmail.com>
2023-04-21 20:43:31 -0700

    Removed modules

--- src/main/java/io/github/itzispyder/clickcrystals/modules/modules/ClickCrystal.java
+++ src/main/java/io/github/itzispyder/clickcrystals/modules/modules/ClickCrystal.java
@@ -2,6 +2,7 @@ package io.github.itzispyder.clickcrystals.modules.modules;
 
 import io.github.itzispyder.clickcrystals.events.EventHandler;
 import io.github.itzispyder.clickcrystals.events.Listener;
+import io.github.itzispyder.clickcrystals.events.events.ClientTickEndEvent;
 import io.github.itzispyder.clickcrystals.events.events.PacketSendEvent;
 import io.github.itzispyder.clickcrystals.modules.Categories;
 import io.github.itzispyder.clickcrystals.modules.Module;
@@ -9,11 +10,11 @@ import io.github.itzispyder.clickcrystals.util.BlockUtils;
 import io.github.itzispyder.clickcrystals.util.HotbarUtils;
 import io.github.itzispyder.clickcrystals.util.InteractionUtils;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
+import net.minecraft.entity.decoration.EndCrystalEntity;
 import net.minecraft.item.Items;
 import net.minecraft.network.packet.c2s.play.PlayerActionC2SPacket;
-import net.minecraft.network.packet.c2s.play.PlayerInteractEntityC2SPacket;
-import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.hit.EntityHitResult;
+import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
 
 /**
@@ -50,11 +51,16 @@ public class ClickCrystal extends Module implements Listener {
                 BlockUtils.interact(pos,packet.getDirection());
             }
         }
-        else if (e.getPacket() instanceof PlayerInteractEntityC2SPacket packet) {
-            final Entity ent = packet.getEntity((ServerWorld) mc.player.getWorld());
-            if (ent == null) return;
-            if (ent.getType() != EntityType.END_CRYSTAL) return;
-            e.setCancelled(true);
+    }
+
+    @EventHandler
+    private void onClientTick(ClientTickEndEvent e) {
+        if (mc.options.useKey.isPressed()) {
+            HitResult hit = mc.crosshairTarget;
+            if (hit == null) return;
+            if (hit.getType() != HitResult.Type.ENTITY) return;
+            Entity ent = ((EntityHitResult) hit).getEntity();
+            if (!(ent instanceof EndCrystalEntity)) return;
             InteractionUtils.doAttack();
         }
     }
