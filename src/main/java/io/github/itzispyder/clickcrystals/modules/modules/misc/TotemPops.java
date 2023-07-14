087be559a8e218e31e80b22955a14e3c8d866605
TheTelly1 <93684527+TheTelly1@users.noreply.github.com>
2023-07-14 00:53:18 -0500

    Added custom text to TotemPops and a color function to StringUtils

--- src/main/java/io/github/itzispyder/clickcrystals/modules/modules/misc/TotemPops.java
+++ src/main/java/io/github/itzispyder/clickcrystals/modules/modules/misc/TotemPops.java
@@ -6,7 +6,9 @@ import io.github.itzispyder.clickcrystals.events.Listener;
 import io.github.itzispyder.clickcrystals.events.events.PacketReceiveEvent;
 import io.github.itzispyder.clickcrystals.modules.Categories;
 import io.github.itzispyder.clickcrystals.modules.Module;
+import io.github.itzispyder.clickcrystals.modules.settings.*;
 import io.github.itzispyder.clickcrystals.util.ChatUtils;
+import io.github.itzispyder.clickcrystals.util.StringUtils;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.network.packet.s2c.play.EntityStatusS2CPacket;
@@ -15,6 +17,32 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TotemPops extends Module implements Listener {
+
+    public final SettingSection general = getGeneralSection();
+    public final ModuleSetting<String> enemyPop = general.add(StringSetting.create()
+            .name("Enemy Pop Message")
+            .description("Message sent when enemy pops, %pops% for count, %player% for their name")
+            .def("&7&n%player% &8&o popped &7&o(&e%pops%&7&o) &8totems!")
+            .build()
+    );
+    public final ModuleSetting<String> enemyDeath = general.add(StringSetting.create()
+            .name("Enemy Death Message")
+            .description("Message sent when enemy pops, %pops% for count, %player% for their name")
+            .def("&7&n%player% &8&o died after popping &7&o(&e%pops%&7&o) &8totems!")
+            .build()
+    );
+    public final ModuleSetting<Boolean> showOwn = general.add(BooleanSetting.create()
+            .name("Show Own")
+            .description("Toggle showing your own pops in chat")
+            .def(true)
+            .build()
+    );
+    public final ModuleSetting<String> ownText = general.add(StringSetting.create()
+            .name("Own pop name")
+            .description("Your name in own pop messages %name% for your name")
+            .def("&6&oYou")
+            .build()
+    );
 
     private static final Map<String,Integer> totemPops = new HashMap<>();
 
@@ -45,14 +73,20 @@ public class TotemPops extends Module implements Listener {
             if (ent.getType() != EntityType.PLAYER) return;
 
             String name = ent.getDisplayName().getString();
-            name = name.equals(mc.player.getDisplayName().getString()) ? "§6§oYou" : name;
+            if (name.equals(mc.player.getDisplayName().getString())) {
+                if (showOwn.getVal()) {
+                    name = StringUtils.color(ownText.getVal().replace("%name%",name));
+                } else {
+                    return;
+                }
+            }
 
             if (status == EntityStatusType.TOTEM_POP) {
                 setPops(ent,getPops(ent) + 1);
-                ChatUtils.sendPrefixMessage("§7§o" + name + "§8§o popped §7§o(§e" + getPops(ent) + "§7§o)");
+                ChatUtils.sendPrefixMessage(StringUtils.color(enemyPop.getVal().replace("%player%",name)));
             }
             else if (status == EntityStatusType.DEATH) {
-                ChatUtils.sendPrefixMessage("§7§o" + name + "§8§o died with §7§o(§6" + getPops(ent) + "§7§o)§8§o pops");
+                ChatUtils.sendPrefixMessage(StringUtils.color(enemyDeath.getVal().replace("%player%",name)));
                 setPops(ent,0);
             }
         }
