658d6bab1b13125d1ccf950b5c72cd436c62cccd
ImproperIssues <ItziSpyder@gmail.com>
2023-07-13 21:15:44 -0700

    blank module settings

--- src/main/java/io/github/itzispyder/clickcrystals/modules/modules/clickcrystals/BlankModule.java
+++ src/main/java/io/github/itzispyder/clickcrystals/modules/modules/clickcrystals/BlankModule.java
@@ -6,87 +6,51 @@ import io.github.itzispyder.clickcrystals.modules.settings.*;
 
 public class BlankModule extends Module {
 
-    private final SettingSection scGeneral = getGeneralSection();
-    private final SettingSection scTest = createSettingSection("hello-world");
-    public final ModuleSetting<Boolean> goofy = scGeneral.add(BooleanSetting.create()
-            .name("goofy-setting")
-            .description("This is a goofy setting from the testing module.")
+    private final SettingSection scGeneral = getGeneralSection(); // this is the default setting section
+    private final SettingSection scTest = createSettingSection("hello-world"); // this is a new setting section
+    public final ModuleSetting<Boolean> boolSetting = scGeneral.add(BooleanSetting.create() // this is a new setting
+            .name("boolean-setting")
+            .description("This is a boolean setting.")
             .def(false)
             .build()
     );
-    public final ModuleSetting<String> anotherString = scGeneral.add(StringSetting.create()
-            .name("lmfao-real")
-            .description("weeeeeeeee")
+    public final ModuleSetting<String> strSetting = scGeneral.add(StringSetting.create()
+            .name("string-setting")
+            .description("This is a string setting.")
             .def("KYS (keep urself safe)")
             .build()
     );
-    public final ModuleSetting<String> anotherAnotherString = scGeneral.add(StringSetting.create()
-            .name("lmfao-real")
-            .description("weeeeeeeee")
-            .def("KYS (keep urself safe)")
-            .build()
-    );
-    public final ModuleSetting<String> anotherStringAnother = scGeneral.add(StringSetting.create()
-            .name("lmfao-real")
-            .description("weeeeeeeee")
-            .def("KYS (keep urself safe)")
-            .build()
-    );
-    public final ModuleSetting<Integer> anotherInt = scGeneral.add(IntegerSetting.create()
+    public final ModuleSetting<Integer> intSettingNoMin = scGeneral.add(IntegerSetting.create()
             .max(10)
             .min(0)
-            .name("testing-integer-setting")
-            .description("this knob does nothing haha lol")
+            .name("integer-setting")
+            .description("This is an integer setting.")
             .def(4)
             .build()
     );
-    public final ModuleSetting<Integer> coolInt = scGeneral.add(IntegerSetting.create()
+    public final ModuleSetting<Integer> intSettingWithMin = scGeneral.add(IntegerSetting.create()
             .max(30)
             .min(5)
-            .name("testing-integer-setting-again")
-            .description("this knob does nothing haha lol again")
+            .name("integer-setting-with-minimum")
+            .description("This is an integer setting with a minimum value.")
             .def(10)
             .build()
     );
-    public final ModuleSetting<Boolean> hello = scTest.add(BooleanSetting.create()
-            .name("hello-world-boolean")
-            .description("Testing 123")
-            .def(false)
-            .build()
-    );
-    public final ModuleSetting<Boolean> hello2 = scTest.add(BooleanSetting.create()
-            .name("hello-world-2")
-            .description("Shout out to HELLO WORLD, we love JAVA!")
-            .def(false)
-            .build()
-    );
-    public final ModuleSetting<Double> yoYoYo = scTest.add(DoubleSetting.create()
+    public final ModuleSetting<Double> doubleSettingTwoDecimal = scTest.add(DoubleSetting.create()
             .max(101.21)
             .min(75.43)
             .decimalPlaces(2)
-            .name("yo-yo-yo-test")
-            .description("Wassup")
+            .name("double-setting-two-decimals")
+            .description("This is a double setting with two decimal places.")
             .def(87.34)
             .build()
     );
-    public final ModuleSetting<Double> yoYoYo2 = scTest.add(DoubleSetting.create()
+    public final ModuleSetting<Double> doubleSettingOneDecimal = scTest.add(DoubleSetting.create()
             .max(34.7)
             .decimalPlaces(1)
-            .name("yo-yo-yo-test-secondary")
-            .description("Wassup [x2]")
+            .name("double-setting-one-decimals")
+            .description("This is a double setting with one decimal places.")
             .def(5.67)
-            .build()
-    );
-    public final ModuleSetting<String> mailBox = scTest.add(StringSetting.create()
-            .name("your-mail-box")
-            .description("You have mail!")
-            .def("Hello World")
-            .build()
-    );
-    public final ModuleSetting<String> eee = scTest.add(StringSetting.create()
-            .name("eee")
-            .description("eeeeeeeeeeeeeeeeeeeeeeeeeee")
-            .def("Hello World with E's")
             .build()
     );
 
