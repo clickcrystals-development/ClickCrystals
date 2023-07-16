489b8bb4d681212c27a68766a520ecc0b823c27e
ImproperIssues <ItziSpyder@gmail.com>
2023-07-16 14:40:25 -0700

    0.9.2 pre release

--- src/main/java/io/github/itzispyder/clickcrystals/ClickCrystals.java
+++ src/main/java/io/github/itzispyder/clickcrystals/ClickCrystals.java
@@ -47,7 +47,7 @@ public final class ClickCrystals implements ModInitializer {
             modId = "clickcrystals",
             prefix = "[ClickCrystals] ",
             starter = "§7[§bClick§3Crystals§7] §r",
-            version = "0.9.1";
+            version = "0.9.2";
 
     /**
      * Runs the mod initializer.
