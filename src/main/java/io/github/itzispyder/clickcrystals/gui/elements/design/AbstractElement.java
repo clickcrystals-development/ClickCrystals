164b60e2b56c5497c637c99d33c55eec267fb056
ImproperIssues <ItziSpyder@gmail.com>
2023-08-12 11:37:13 -0700

    improved search screen and made gui accessible via commands ,toggle

--- src/main/java/io/github/itzispyder/clickcrystals/gui/elements/design/AbstractElement.java
+++ src/main/java/io/github/itzispyder/clickcrystals/gui/elements/design/AbstractElement.java
@@ -33,4 +33,67 @@ public class AbstractElement extends GuiElement {
     public PressAction<AbstractElement> getPressAction() {
         return onPress;
     }
+
+    public static Builder create() {
+        return new Builder();
+    }
+
+
+    public static class Builder {
+        private int x, y, width, height;
+        private RenderAction<AbstractElement> onRender;
+        private PressAction<AbstractElement> onPress;
+
+        public Builder() {
+            onRender = (context, mouseX, mouseY, button) -> {};
+            onPress = button -> {};
+            x = y = width = height = 0;
+        }
+
+        public Builder x(int x) {
+            this.x = x;
+            return this;
+        }
+
+        public Builder y(int y) {
+            this.y = y;
+            return this;
+        }
+
+        public Builder pos(int x, int y) {
+            this.x = x;
+            this.y = y;
+            return this;
+        }
+
+        public Builder width(int width) {
+            this.width = width;
+            return this;
+        }
+
+        public Builder height(int height) {
+            this.height = height;
+            return this;
+        }
+
+        public Builder dimensions(int width, int height) {
+            this.width = width;
+            this.height = height;
+            return this;
+        }
+
+        public Builder onRender(RenderAction<AbstractElement> onRender) {
+            this.onRender = onRender;
+            return this;
+        }
+
+        public Builder onPress(PressAction<AbstractElement> onPress) {
+            this.onPress = onPress;
+            return this;
+        }
+
+        public AbstractElement build() {
+            return new AbstractElement(x, y, width, height, onRender, onPress);
+        }
+    }
 }
