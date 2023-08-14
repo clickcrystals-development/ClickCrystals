ebd9dbc7c9d499588cbf0d70680231b74bdf86cf
Tutla <91965613+ayaanibrahimtutla@users.noreply.github.com>
2023-08-14 18:27:22 +0530

    Update ClickCrystalsSystem.java
    Added a few comments to make code reading easier for anyone want to understand it

--- src/main/java/io/github/itzispyder/clickcrystals/client/system/ClickCrystalsSystem.java
+++ src/main/java/io/github/itzispyder/clickcrystals/client/system/ClickCrystalsSystem.java
@@ -1,9 +1,9 @@
 package io.github.itzispyder.clickcrystals.client.system;
-
+// Imports
 import io.github.itzispyder.clickcrystals.commands.Command;
 import io.github.itzispyder.clickcrystals.events.EventBus;
 import io.github.itzispyder.clickcrystals.events.Listener;
-import io.github.itzispyder.clickcrystals.modules.Module;
+import io.github.itzispyder.clickcrystals.modules.Module;https://github.com/ItziSpyder/ClickCrystals/pulls
 import io.github.itzispyder.clickcrystals.modules.keybinds.Keybind;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.gui.screen.ConfirmLinkScreen;
@@ -15,7 +15,7 @@ import java.io.Serializable;
 import java.util.*;
 
 import static io.github.itzispyder.clickcrystals.ClickCrystals.mc;
-
+// The main thing
 public class ClickCrystalsSystem implements Serializable {
 
     private static final ClickCrystalsSystem system = new ClickCrystalsSystem();
@@ -36,7 +36,7 @@ public class ClickCrystalsSystem implements Serializable {
         this.huds = new HashMap<>();
         this.keybinds = new HashSet<>();
     }
-
+    // Functions to open Url
     public void openUrl(String url) {
         this.openUrl(url, "Are you sure you want to go to the following link?", mc.currentScreen);
     }
@@ -53,36 +53,36 @@ public class ClickCrystalsSystem implements Serializable {
             mc.setScreen(afterScreen);
         }, Text.literal(title), url, true));
     }
-
+    // Add commands using this function (import and use the funtion)
     public void addCommand(Command command) {
         if (command == null) return;
         commands.put(command.getClass(), command);
         command.register();
     }
-
+    // Add Modules using this function (import and use the funtion)
     public void addModule(Module module) {
         if (module == null) return;
         modules.put(module.getClass(),module);
     }
-
+    // Add HUDs using this function (import and use the funtion)
     public void addHud(HudRenderCallback hudRenderer) {
         HudRenderCallback.EVENT.register(hudRenderer);
         huds.put(hudRenderer.getClass(), hudRenderer);
     }
-
+    // Add keybinds using this function (import and use the funtion)
     public void addKeybind(Keybind bind) {
         if (bind == null) return;
         this.keybinds.add(bind);
     }
-
+    // Remove keybinds using this function (import and use the funtion)
     public void removeKeybind(Keybind bind) {
         this.keybinds.remove(bind);
     }
-
+    // Adds a listener ( A listener is something that recives a certain input )
     public void addListener(Listener listener) {
         eventBus.subscribe(listener);
     }
-
+    // Removes Listners
     public void removeListener(Listener listener) {
         eventBus.unsubscribe(listener);
     }
