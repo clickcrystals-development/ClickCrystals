24c6b009ecbb878962ab0f0cf91b916e66bbeeaf
CatchyFrown <148023926+CatchyFrown@users.noreply.github.com>
2023-10-26 16:49:20 -0300

    Update LoadingIconElement.java

--- src/main/java/io/github/itzispyder/clickcrystals/gui_beta/elements/display/LoadingIconElement.java
+++ src/main/java/io/github/itzispyder/clickcrystals/gui_beta/elements/display/LoadingIconElement.java
@@ -3,8 +3,9 @@ package io.github.itzispyder.clickcrystals.gui_beta.elements.display;
 import io.github.itzispyder.clickcrystals.gui_beta.GuiElement;
 import io.github.itzispyder.clickcrystals.gui_beta.misc.Tex;
 import io.github.itzispyder.clickcrystals.util.RenderUtils;
-import net.minecraft.client.gui.DrawContext;
-import net.minecraft.util.math.RotationAxis;
+import net.minecraft.client.gui.DrawableHelper;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.math.Rotation;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -14,15 +15,15 @@ public class LoadingIconElement extends GuiElement {
 
     public LoadingIconElement(int x, int y, int size) {
         super(x, y, size, size);
-        system.scheduler.runRepeatingTask(rotation::getAndIncrement, 0, 1, 3000);
     }
 
     @Override
-    public void onRender(DrawContext context, int mouseX, int mouseY) {
-        context.getMatrices().push();
-        context.getMatrices().multiply(RotationAxis.POSITIVE_Z.rotationDegrees(rotation.get()), x + width / 2.0F, y + height / 2.0F, 0);
-        RenderUtils.drawTexture(context, Tex.Icons.LOADING, x, y, width, height);
-        context.getMatrices().pop();
+    public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+        matrices.push();
+        matrices.translate(x + width / 2.0F, y + height / 2.0F, 0);
+        matrices.multiply(Rotation.of(rotation.get(), 0, 0, 1).getMatrix4f());
+        RenderUtils.drawTexture(matrices, Tex.Icons.LOADING, x, y, width, height);
+        matrices.pop();
     }
 
     @Override
