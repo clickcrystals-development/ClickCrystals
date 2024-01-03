2ea3ca7ec7eef8e72b855eeed9d1cab13a5c286d
ImNotStable <jeremiahhughes06@gmail.com>
2024-01-03 02:20:55 -0600

    Added PlayerRespawnEvent

--- src/main/java/io/github/itzispyder/clickcrystals/mixins/MixinClientConnection.java
+++ src/main/java/io/github/itzispyder/clickcrystals/mixins/MixinClientConnection.java
@@ -2,6 +2,7 @@ package io.github.itzispyder.clickcrystals.mixins;
 
 import io.github.itzispyder.clickcrystals.Global;
 import io.github.itzispyder.clickcrystals.events.events.client.EntityDamageEvent;
+import io.github.itzispyder.clickcrystals.events.events.client.PlayerRespawnEvent;
 import io.github.itzispyder.clickcrystals.events.events.networking.GameJoinEvent;
 import io.github.itzispyder.clickcrystals.events.events.networking.PacketReceiveEvent;
 import io.github.itzispyder.clickcrystals.events.events.networking.PacketSendEvent;
@@ -11,6 +12,7 @@ import net.minecraft.network.listener.PacketListener;
 import net.minecraft.network.packet.Packet;
 import net.minecraft.network.packet.s2c.login.LoginSuccessS2CPacket;
 import net.minecraft.network.packet.s2c.play.EntityDamageS2CPacket;
+import net.minecraft.network.packet.s2c.play.PlayerRespawnS2CPacket;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -39,6 +41,9 @@ public abstract class MixinClientConnection implements Global {
         }
         if (packet instanceof EntityDamageS2CPacket p) {
             system.eventBus.pass(new EntityDamageEvent(p));
+        }
+        if (packet instanceof PlayerRespawnS2CPacket p) {
+            system.eventBus.pass(new PlayerRespawnEvent(p));
         }
         system.eventBus.passWithCallbackInfo(ci, new PacketReceiveEvent(packet));
     }
