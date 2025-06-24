9818b491e373b5251b337d6fb635e79a83768b49
I-No-oNe <novonoam76@gmail.com>
2025-06-24 13:06:56 +0300

    Add GUI item highlight and slot draw event

--- src/main/java/io/github/itzispyder/clickcrystals/modules/modules/rendering/ItemHighlight.java
+++ src/main/java/io/github/itzispyder/clickcrystals/modules/modules/rendering/ItemHighlight.java
@@ -1,9 +1,12 @@
 package io.github.itzispyder.clickcrystals.modules.modules.rendering;
 
 import io.github.itzispyder.clickcrystals.events.EventHandler;
+import io.github.itzispyder.clickcrystals.events.events.world.DrawSlotEvent;
 import io.github.itzispyder.clickcrystals.events.events.world.RenderWorldEvent;
 import io.github.itzispyder.clickcrystals.modules.Categories;
+import io.github.itzispyder.clickcrystals.modules.ModuleSetting;
 import io.github.itzispyder.clickcrystals.modules.modules.ListenerModule;
+import io.github.itzispyder.clickcrystals.modules.settings.SettingSection;
 import io.github.itzispyder.clickcrystals.util.MathUtils;
 import io.github.itzispyder.clickcrystals.util.minecraft.PlayerUtils;
 import io.github.itzispyder.clickcrystals.util.minecraft.render.RenderUtils3d;
@@ -23,18 +26,56 @@ public class ItemHighlight extends ListenerModule {
         super("item-highlight", Categories.RENDER, "Renders a highlight around dropped items to make them more visible");
     }
 
+    public final SettingSection general = getGeneralSection();
+
+    ModuleSetting<RenderType> renderType = general.add(createEnumSetting(RenderType.class)
+            .name("Render Type")
+            .description("Where to render the highlight")
+            .def(RenderType.BOTH)
+            .build()
+    );
+
+    ModuleSetting<Rarity> rarityFilter = general.add(createEnumSetting(Rarity.class)
+            .name("Rarity Filter")
+            .description("Only render items of this rarity or higher")
+            .def(Rarity.COMMON)
+            .build()
+    );
+
     @EventHandler
     public void onRenderWorld(RenderWorldEvent e) {
+        if (!renderType.getVal().renderWorld() || !isEnabled())
+            return;
+
         MatrixStack matrices = e.getMatrices();
         float tickDelta = e.getTickCounter().getTickProgress(true);
 
         for (ItemEntity itemEntity : getItemEntities()) {
             ItemStack item = itemEntity.getStack();
             Vec3d pos = e.getOffsetPos(MathUtils.lerpEntityPosVec(itemEntity, tickDelta));
-            int color = getRarityColor(item.getRarity());
+            Rarity rarity = item.getRarity();
+
+            if (rarity.ordinal() < rarityFilter.getVal().ordinal())
+                continue;
+
+            int color = getRarityColor(rarity);
             int fadeColor = 0x00FFFFFF & color;
             RenderUtils3d.fillCylGradient(matrices, pos.x, pos.y, pos.z, 0.2, 0.25, color, fadeColor);
         }
+    }
+
+    @EventHandler
+    public void onDrawSlot(DrawSlotEvent e) {
+        if (!isEnabled() && !renderType.getVal().renderGui() && !e.getSlot().hasStack()) return;
+
+        if (e.getSlot().getStack().getRarity().ordinal() < rarityFilter.getVal().ordinal()) return;
+
+
+        int color = getRarityColor(e.getSlot().getStack().getRarity());
+        int x = e.getSlot().x;
+        int y = e.getSlot().y;
+
+        e.getContext().fill(x, y, x + 16, y + 16, color);
     }
 
     public List<ItemEntity> getItemEntities() {
@@ -45,7 +86,7 @@ public class ItemHighlight extends ListenerModule {
         return list;
     }
 
-    private int getRarityColor(Rarity rarity) {
+    public int getRarityColor(Rarity rarity) {
         switch (rarity) {
             case COMMON -> {
                 return 0xFFFFFFFF;
@@ -62,6 +103,20 @@ public class ItemHighlight extends ListenerModule {
             case null, default -> {
                 return 0xFFAAAAAA;
             }
+        }
+    }
+
+    enum RenderType {
+        GUI,
+        WORLD,
+        BOTH;
+
+        boolean renderGui() {
+            return this == GUI || this == BOTH;
+        }
+
+        boolean renderWorld() {
+            return this == WORLD || this == BOTH;
         }
     }
 }
